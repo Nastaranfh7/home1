c661011699ee6459ae6cd941444167658bfee7e7
Nastaranfh7 <149198756+Nastaranfh7@users.noreply.github.com>
2024-05-23 22:19:23 +0330

    Add files via upload

---
+++ javahm.js
@@ -0,0 +1,21 @@
+ // Declare variables
+ let city = "Tokyo";
+ let region = "Kanto";
+ let population_density = 6156; // people per square kilometer
+ let area = 2187; // square kilometers
+
+ // Log values to the console
+ console.log("City:", city);
+ console.log("Region:", region);
+ console.log("Population Density:", population_density);
+ console.log("Area:", area);
+
+ // Declare variables
+ let isCapital = true;
+ let officialLanguage;
+
+ // Log types to the console
+ console.log("Type of isCapital:", typeof isCapital);
+ console.log("Type of population_density:", typeof population_density);
+ console.log("Type of city:", typeof city);
+ console.log("Type of officialLanguage:", typeof officialLanguage);
